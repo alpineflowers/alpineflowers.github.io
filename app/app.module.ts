7562a70e0c6296b5b853721fbf3ab7290aa4e461
alpineflowers <49690393+alpineflowers@users.noreply.github.com>
2019-04-16 17:16:43 +0100

    Test

---
+++ app/app.module.ts
@@ -0,0 +1,46 @@
+import { NgModule, ErrorHandler } from '@angular/core';
+import { BrowserModule } from '@angular/platform-browser';
+import { IonicApp, IonicModule, IonicErrorHandler } from 'ionic-angular';
+import { MyApp } from './app.component';
+import { HomePage } from '../pages/home/home';
+import { CartTabDefaultPagePage } from '../pages/cart-tab-default-page/cart-tab-default-page';
+import { CloudTabDefaultPagePage } from '../pages/cloud-tab-default-page/cloud-tab-default-page';
+import { TabsControllerPage } from '../pages/tabs-controller/tabs-controller';
+import { SettingsPage } from '../pages/settings/settings';
+import { ResultsPage } from '../pages/results/results';
+
+
+import { StatusBar } from '@ionic-native/status-bar';
+import { SplashScreen } from '@ionic-native/splash-screen';
+
+@NgModule({
+  declarations: [
+    MyApp,
+    HomePage,
+    CartTabDefaultPagePage,
+    CloudTabDefaultPagePage,
+    TabsControllerPage,
+    SettingsPage,
+    ResultsPage
+  ],
+  imports: [
+    BrowserModule,
+    IonicModule.forRoot(MyApp)
+  ],
+  bootstrap: [IonicApp],
+  entryComponents: [
+    MyApp,
+    HomePage,
+    CartTabDefaultPagePage,
+    CloudTabDefaultPagePage,
+    TabsControllerPage,
+    SettingsPage,
+    ResultsPage
+  ],
+  providers: [
+    StatusBar,
+    SplashScreen,
+    {provide: ErrorHandler, useClass: IonicErrorHandler}
+  ]
+})
+export class AppModule {}
